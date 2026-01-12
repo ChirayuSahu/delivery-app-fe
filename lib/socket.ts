2942f16ca85c0cbf553a506e5edcf4f8b7a7d59c
Chirayu Sahu <chirayuajaysahu01@gmail.com>
2026-01-12 21:30:30 +0530

    feat(socket): enable withCredentials for socket connection

--- lib/socket.ts
+++ lib/socket.ts
@@ -6,6 +6,7 @@ export function getSocket() {
 
     if (!socket) {
         socket = io(process.env.NEXT_PUBLIC_BACKEND_URL!, {
+            withCredentials: true,
             transports: ['websocket'],
             autoConnect: false,
         })
